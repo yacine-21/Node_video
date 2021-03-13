16feebfc04f6183a24bb3d1a2bd88318a6d70ff4
Yacine Lyoubi <yacinelyoubi37@gmail.com>
2021-03-13 01:14:32 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello world")
+
+let msg = "Bonjour le monde !"
+
+console.log(msg)
+
+let fruits = ["banane","pomme"]
+console.log(fruits); 
+
+fruits.push("poire");
+console.log(fruits); 
+
